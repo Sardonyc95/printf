cc4cd984a8715faa3f11ac07125badec32467662
Amavic <amarachi5695@gmail.com>
2023-09-18 13:12:58 -0700

    I am not going anywhere. You can print that wherever you want to. I am here and I am a Spur for life

---
+++ 0-printf.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Function that prints output using Printf function
+ * @format: input param (format).
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	int i, print = 0, print_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			print_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			print = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (print == -1)
+				return (-1);
+			print_chars += print;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (print_chars);
+}
+
+/**
+ * print_buffer - Function that prints the contents of the buffer if it exist
+ * @buffer: input param (Array of chars)
+ * @buff_ind: input param (Index at which to add next char, represents
+ * the length).
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
